461d3c969b0b5e75227417d60cf4076bde660da1
Alex <alexbrunoalexandrino@gmail.com>
2023-07-29 18:53:12 -0400

    knowledge: fix vercel try 2

--- app/components/pages/home/known-techs/index.tsx
+++ app/components/pages/home/known-techs/index.tsx
@@ -1,12 +1,15 @@
-import { SectionTitle } from "@/app/components/section-title"
-import { TbBrandNextjs } from "react-icons/tb"
-import { KnownTech } from "./known-tech"
+/* eslint-disable react/jsx-no-target-blank */
+/* eslint-disable react/jsx-key */
+/* eslint-disable prettier/prettier */
+/* line 13 - os cards irão ter 264px, menos disse a coluna de cards diminui*/
+import { SectionTitle } from '@/app/components/section-title'
+import { TbBrandNextjs } from 'react-icons/tb'
+import { KnownTech } from './known-tech'
 
 export const KnownTechs = () => {
     return (
         <section className="container py-16">
-            <SectionTitle subtitle="competências" title='Cohecimentos'/>
-                                            {/*os cards irão ter 264px, menos disse a coluna de cards diminui*/}
+            <SectionTitle subtitle="competências" title="Cohecimentos"/>
             <div className="grid grid-cols-[repeat(auto-fit,minmax(264px,1fr))] gap-3 mt-[60px]">
                 {Array.from({length: 8}).map((_, index) => (
                     <KnownTech key={index} tech={{

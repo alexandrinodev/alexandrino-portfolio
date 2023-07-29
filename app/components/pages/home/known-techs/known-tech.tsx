e03abc179a4457d13de07159bd33b603b4069cd9
Alex <alexbrunoalexandrino@gmail.com>
2023-07-29 19:24:28 -0400

    knowledge: fix vercel try 5

--- app/components/pages/home/known-techs/known-tech.tsx
+++ app/components/pages/home/known-techs/known-tech.tsx
@@ -1,8 +1,4 @@
-<<<<<<< HEAD
 import { ReactNode } from 'react'
-=======
-import { ReactNode } from "react"
->>>>>>> 51d131072452e7ca3e0b61c71b68ce7c51aba110
 
 type KnownTechProps = {
   tech: {
@@ -12,7 +8,6 @@ type KnownTechProps = {
   }
 }
 
-<<<<<<< HEAD
 export const KnownTech = ({ tech }: KnownTechProps) => {
   return (
     <div className="p-6 hover:scale-110 rounded-lg bg-gray-600/20 text-gray-500 flex flex-col gap-2 hover:text-emerald-500 hover:bg-gray-600/30 transition-all">
@@ -25,17 +20,3 @@ export const KnownTech = ({ tech }: KnownTechProps) => {
     </div>
   )
 }
-=======
-export const KnownTech = ({tech}: KnownTechProps) => {
-    return (
-        <div className="p-6 hover:scale-110 rounded-lg bg-gray-600/20 text-gray-500 flex flex-col gap-2 hover:text-emerald-500 hover:bg-gray-600/30 transition-all">
-            <div className="flex items-center justify-between">
-                <p className="font-medium">{tech.name}</p>
-                {tech.icon}
-            </div>
-
-            <span>de experiência</span>
-        </div>
-    )
-}
->>>>>>> 51d131072452e7ca3e0b61c71b68ce7c51aba110
